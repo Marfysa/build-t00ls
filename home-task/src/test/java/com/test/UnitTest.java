8b6e6ae055e1ac9c8bf1635f48b8099036dab1a9
Maria <marfysa14@gmail.com>
2022-03-23 12:28:45 +0300

    del bug

--- home-task/src/test/java/com/test/UnitTest.java
+++ home-task/src/test/java/com/test/UnitTest.java
@@ -15,15 +15,3 @@ public class UnitTest {
      Project.test();
    }
 }
-public class ForLoopInfiniteExample {
-
-	public static void main(String[] args) {
-
-		// for loop condition to true.
-		// this loop runs forever.
-		for (; true;) {
-			// core logic
-			System.out.println("Running loop");
-		}
-	}
-}
